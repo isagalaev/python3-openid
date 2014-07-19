c638afb59aa7a0ae77069b81654d9f60cb258b72
Ivan Sagalaev <maniac@softwaremaniacs.org>
2014-07-18 22:31:58 -0700

    Generating test methods within a TestCase class instead of generating test class instances and returning them in a custom suite.

--- openid/test/test_yadis_discover.py
+++ openid/test/test_yadis_discover.py
@@ -23,6 +23,16 @@ STATUS_RE = re.compile(r'^Status: (\d+) .+\n')
 BASE_URL = 'http://invalid.unittest/'
 
 
+class TestSecondGet(unittest.TestCase):
+    def test_404(self):
+        location = 'http://unittest/404'
+        fetch = mock.Mock(return_value=HTTPResponse('', 200, {'X-XRDS-Location': location}))
+        with mock.patch('openid.fetchers.fetch', fetch):
+            discover('http://something.unittest/')
+        fetch.assert_called_with(location)
+        self.assertEqual(fetch.call_count, 2)
+
+
 def mkResponse(data):
     status = int(STATUS_RE.search(data).group(1))
     headers_str, body = data.split('\n\n', 1)
@@ -50,94 +60,61 @@ def fetch(url, body=None, headers=None):
             return response
 
 
-class TestSecondGet(unittest.TestCase):
-    def test_404(self):
-        location = 'http://unittest/404'
-        fetch = mock.Mock(return_value=HTTPResponse('', 200, {'X-XRDS-Location': location}))
-        with mock.patch('openid.fetchers.fetch', fetch):
-            discover('http://something.unittest/')
-        fetch.assert_called_with(location)
-        self.assertEqual(fetch.call_count, 2)
-
-
-class _TestCase(unittest.TestCase):
-    def __init__(self, input_name, id_name, result_name, success):
-        self.input_name = input_name
-        self.id_name = id_name
-        self.result_name = result_name
-        self.success = success
-        # Still not quite sure how to best construct these custom tests.
-        # Between python2.3 and python2.4, a patch attached to pyunit.sf.net
-        # bug #469444 got applied which breaks loadTestsFromModule on this
-        # class if it has test_ or runTest methods.  So, kludge to change
-        # the method name.
-        unittest.TestCase.__init__(self, methodName='runCustomTest')
-
-    def setUp(self):
-        self.input_url, self.expected = discoverdata.generateResult(
-            BASE_URL,
-            self.input_name,
-            self.id_name,
-            self.result_name,
-            self.success)
+class Discover(unittest.TestCase):
 
     @mock.patch('openid.fetchers.fetch', fetch)
-    def runCustomTest(self):
-        if self.expected is urllib.error.HTTPError:
-            self.assertRaises(urllib.error.HTTPError,
-                                  discover, self.input_url)
+    def _test(self, success, input_name, id_name, result_name):
+        '''
+        Common function called with different arguments from generated
+        test methods.
+        '''
+        input_url, expected = discoverdata.generateResult(
+            BASE_URL,
+            input_name,
+            id_name,
+            result_name,
+            success,
+        )
+        if expected is urllib.error.HTTPError:
+            self.assertRaises(urllib.error.HTTPError, discover, input_url)
         else:
-            result = discover(self.input_url)
-            self.assertEqual(self.input_url, result.request_uri)
+            result = discover(input_url)
+            self.assertEqual(input_url, result.request_uri)
 
             msg = 'Identity URL mismatch: actual = %r, expected = %r' % (
-                result.normalized_uri, self.expected.normalized_uri)
+                result.normalized_uri, expected.normalized_uri)
             self.assertEqual(
-                self.expected.normalized_uri, result.normalized_uri, msg)
+                expected.normalized_uri, result.normalized_uri, msg)
 
             msg = 'Content mismatch: actual = %r, expected = %r' % (
-                result.response_text, self.expected.response_text)
+                result.response_text, expected.response_text)
             self.assertEqual(
-                self.expected.response_text, result.response_text, msg)
+                expected.response_text, result.response_text, msg)
 
-            expected_keys = dir(self.expected)
+            expected_keys = dir(expected)
             expected_keys.sort()
             actual_keys = dir(result)
             actual_keys.sort()
             self.assertEqual(actual_keys, expected_keys)
 
-            for k in dir(self.expected):
+            for k in dir(expected):
                 if k.startswith('__') and k.endswith('__'):
                     continue
-                exp_v = getattr(self.expected, k)
+                exp_v = getattr(expected, k)
                 if isinstance(exp_v, types.MethodType):
                     continue
                 act_v = getattr(result, k)
                 assert act_v == exp_v, (k, exp_v, act_v)
 
-    def shortDescription(self):
-        try:
-            n = self.input_url
-        except AttributeError:
-            # run before setUp, or if setUp did not complete successfully.
-            n = self.input_name
-        return "%s (%s)" % (
-            n,
-            self.__class__.__module__)
+# Generation of test methods within Discover. They have predictable names,
+# can be run individually and are discovered by standard unittest machinery.
+for success, input_name, id_name, result_name in discoverdata.testlist:
+    def test_method(self):
+        self._test(success, input_name, id_name, result_name)
+    name = 'test_%s' % input_name
+    test_method.__name__ = name
+    setattr(Discover, name, test_method)
 
-
-def pyUnitTests():
-    s = unittest.TestSuite()
-    for success, input_name, id_name, result_name in discoverdata.testlist:
-        test = _TestCase(input_name, id_name, result_name, success)
-        s.addTest(test)
-
-    return s
-
-
-def test():
-    runner = unittest.TextTestRunner()
-    return runner.run(pyUnitTests())
 
 if __name__ == '__main__':
-    test()
+    unittest.main()
