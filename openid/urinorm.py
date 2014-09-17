c75748c2e4c9cb8c736754d2afc40661d48c5b5e
Ivan Sagalaev <maniac@softwaremaniacs.org>
2014-09-17 00:23:46 -0700

    Cleaned up constants declaration in urinorm.

--- openid/urinorm.py
+++ openid/urinorm.py
@@ -2,34 +2,18 @@ import re
 import urllib.parse
 
 
-uri_illegal_char_re = re.compile(
-    "[^-A-Za-z0-9:/?#[\]@!$&'()*+,;=._~%]", re.UNICODE)
-
-authority_re = re.compile(r'^([^@]*@)?([^:]*)(:.*)?')
-
-pct_encoded_pattern = r'%([0-9A-Fa-f]{2})'
-pct_encoded_re = re.compile(pct_encoded_pattern)
-
-_unreserved = [False] * 256
-for _ in range(ord('A'), ord('Z') + 1): _unreserved[_] = True
-for _ in range(ord('0'), ord('9') + 1): _unreserved[_] = True
-for _ in range(ord('a'), ord('z') + 1): _unreserved[_] = True
-_unreserved[ord('-')] = True
-_unreserved[ord('.')] = True
-_unreserved[ord('_')] = True
-_unreserved[ord('~')] = True
-
+AUTHORITY_RE = re.compile(r'^([^@]*@)?([^:]*)(:.*)?')
+PCT_ENCODED_RE = re.compile(r'%([0-9A-Fa-f]{2})')
+UNRESERVED = '-._~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 ASCII = ''.join(map(chr, range(256)))
 
 
 def _pct_encoded_replace_unreserved(mo):
     try:
-        i = int(mo.group(1), 16)
-        if _unreserved[i]:
-            return chr(i)
-        else:
-            return mo.group().upper()
-
+        c = chr(int(mo.group(1), 16))
+        if c in UNRESERVED:
+            return c
+        return mo.group().upper()
     except ValueError:
         return mo.group()
 
@@ -85,7 +69,7 @@ def urinorm(uri):
     if not scheme or not authority or scheme not in ('http', 'https'):
         raise ValueError('Not an absolute HTTP or HTTPS URI: %s' % uri)
 
-    if not authority_re.match(authority):
+    if not AUTHORITY_RE.match(authority):
         raise ValueError('URI does not have a valid authority: %s' % uri)
 
     # This should've been simply authority.encode(..).decode(..) but idna breaks on
@@ -96,7 +80,7 @@ def urinorm(uri):
         if not port or (scheme, port) in [('http', '80'), ('https', '443')]:
             authority = host
 
-    path = pct_encoded_re.sub(_pct_encoded_replace_unreserved, path)
+    path = PCT_ENCODED_RE.sub(_pct_encoded_replace_unreserved, path)
     path = remove_dot_segments(path)
     if not path:
         path = '/'
